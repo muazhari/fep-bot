a199ce2af9b8ee753fc2255f61846bdbf013b3dd
Glitch (fep-bot) <none>
2019-07-24 09:05:07 +0000

    🎈😀 Alpha release

--- src/Bot/Features/internal.js
+++ src/Bot/Features/internal.js
@@ -2,4 +2,3 @@ export * from "./Access";
 export * from "./Basic";
 export * from "./FEPList";
 export * from "./StoreAdvance";
-// export * from "../../Bot";
--- src/Bot/internal.js
+++ src/Bot/internal.js
@@ -1,9 +1,3 @@
 export * from "./Bot";
 export * from "./Handler"
 export * from "./Features";
-
-// export * from "./Features/Access";
-// export * from "./Features/Basic";
-// export * from "./Features/FEPList";
-// export * from "./Features/StoreAdvance";
-// export * from "./Bot";
