6b83e6625a51bbd0885d1cf520e0a7c1339deab1
Glitch (fep-bot) <none>
2019-11-29 22:00:00 +0000

    ⛴🍠 Checkpoint ./src/Services/Store.js:5658260/753

--- dist/Services/Store.js
+++ dist/Services/Store.js
@@ -1,10 +1,10 @@
-"use strict";
+'use strict';
 
 Object.defineProperty(exports, "__esModule", {
-  value: true
+   value: true
 });
 
-var _nodePersist = require("node-persist");
+var _nodePersist = require('node-persist');
 
 var _nodePersist2 = _interopRequireDefault(_nodePersist);
 
@@ -13,26 +13,50 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 // share worker props by groupId
 // const shared_props = storage.create({});
 
+const initLogStoreage = async () => {
+   await _nodePersist2.default.create({
+      dir: 'relative/path/to/persist',
+
+      stringify: JSON.stringify,
+
+      parse: JSON.parse,
+
+      encoding: 'utf8',
+
+      logging: false, // can also be custom logging function
+
+      ttl: false, // ttl* [NEW], can be true for 24h default or a number in MILLISECONDS or a valid Javascript Date object
+
+      expiredInterval: 2 * 60 * 1000, // every 2 minutes the process will clean-up the expired cache
+
+      // in some cases, you (or some other service) might add non-valid storage files to your
+      // storage dir, i.e. Google Drive, make this true if you'd like to ignore these files and not throw an error
+      forgiveParseErrors: false
+
+   });
+};
+
 const init = async () => {
-  await _nodePersist2.default.init();
+   await _nodePersist2.default.init();
+   await initLogStorage();
 };
 
 const setStore = async obj => {
-  Object.keys(obj).map(async key => {
-    await _nodePersist2.default.setItem(key, obj[key]);
-  });
+   await Object.keys(obj).map(async key => {
+      await _nodePersist2.default.setItem(key, obj[key]);
+   });
 };
 
 const getStore = async storeName => {
-  return await _nodePersist2.default.getItem(storeName);
+   return await _nodePersist2.default.getItem(storeName);
 };
 
 const remStore = async storeName => {
-  await _nodePersist2.default.removeItem(storeName);
+   await _nodePersist2.default.removeItem(storeName);
 };
 
 const updStore = async (storeName, value) => {
-  await _nodePersist2.default.updateItem(storeName, value);
+   await _nodePersist2.default.updateItem(storeName, value);
 };
 
 exports.default = { init, setStore, getStore, remStore, updStore };
--- src/Services/Store.js
+++ src/Services/Store.js
@@ -3,12 +3,36 @@ import storage from "node-persist";
 // share worker props by groupId
 // const shared_props = storage.create({});
 
+const initLogStoreage = async () =>{
+  await storage.create({
+    dir: 'relative/path/to/persist',
+ 
+    stringify: JSON.stringify,
+ 
+    parse: JSON.parse,
+ 
+    encoding: 'utf8',
+ 
+    logging: false,  // can also be custom logging function
+ 
+    ttl: false, // ttl* [NEW], can be true for 24h default or a number in MILLISECONDS or a valid Javascript Date object
+ 
+    expiredInterval: 2 * 60 * 1000, // every 2 minutes the process will clean-up the expired cache
+ 
+    // in some cases, you (or some other service) might add non-valid storage files to your
+    // storage dir, i.e. Google Drive, make this true if you'd like to ignore these files and not throw an error
+    forgiveParseErrors: false
+ 
+});
+}
+
 const init = async () => {
   await storage.init();
+  await initLogStorage();
 };
 
 const setStore = async obj => {
-  Object.keys(obj).map(async key => {
+  await Object.keys(obj).map(async key => {
     await storage.setItem(key, obj[key]);
   });
 };
