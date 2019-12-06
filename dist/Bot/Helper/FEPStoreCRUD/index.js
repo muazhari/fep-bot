a01c55fa9eaac03b29cb48aa401e03edb9cb2f50
Glitch (fep-bot) <none>
2019-12-06 04:22:36 +0000

    👘🐜 Checkpoint ./src/Bot/Helper/FEPStoreCRUD/index.js:5658260/10043

--- dist/Bot/Helper/FEPStoreCRUD/index.js
+++ dist/Bot/Helper/FEPStoreCRUD/index.js
@@ -17,25 +17,35 @@ var _Store2 = _interopRequireDefault(_Store);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 const set_store = async data => {
-  const store = await _Store2.default.getStore("fep");
-  if (Object.keys(_Bot.batch_list).includes(data.batch)) {
-    if (!Object.keys(store).includes(data.batch)) {
-      store[data.batch] = [];
+  return new Promise(async (resolve, reject) => {
+    if (!Object.keys(_Bot.batch_list).includes(data.batch)) {
+      return reject("Not in a proper batch");
+    }
+
+    let store = await _Store2.default.getStore("fep");
+    if (store === undefined) {
+      store = { [data.batch]: [] };
     }
 
     const selected_user_data = [data.name, data.campus, data.room];
     store[data.batch].push(selected_user_data);
     await _Store2.default.setStore({ fep: store });
-  }
+    return resolve();
+  });
 };
 
 const update_store = async data => {
-  const store = await _Store2.default.getStore("fep");
-  if (Object.keys(_Bot.batch_list).includes(data.batch)) {
-    const selected_user_data = [data.name, data.campus, data.room];
-    store[data.batch][parseInt(data.num, 10) - 1] = selected_user_data;
+  return new Promise(async (resolve, reject) => {
+    if (!Object.keys(_Bot.batch_list).includes(data.batch)) {
+      return reject("Not in a proper batch");
+    }
+
+    const store = await _Store2.default.getStore("fep");
+    const selectedUserData = [data.name, data.campus, data.room];
+    store[data.batch][parseInt(data.num, 10) - 1] = selectedUserData;
     await _Store2.default.setStore({ fep: store });
-  }
+    return resolve();
+  });
 };
 
 const delete_store = async data => {
--- src/Bot/Helper/FEPStoreCRUD/index.js
+++ src/Bot/Helper/FEPStoreCRUD/index.js
@@ -3,25 +3,35 @@ import storage from "node-persist";
 import Store from "../../../Services/Store";
 
 const set_store = async data => {
-  const store = await Store.getStore("fep");
-  if (Object.keys(batch_list).includes(data.batch)) {
-    if (!Object.keys(store).includes(data.batch)) {
-      store[data.batch] = [];
+  return new Promise(async (resolve, reject) => {
+    if (!Object.keys(batch_list).includes(data.batch)) {
+      return reject("Not in a proper batch");
+    }
+
+    let store = await Store.getStore("fep");
+    if (store === undefined) {
+      store = { [data.batch]: [] };
     }
 
     const selected_user_data = [data.name, data.campus, data.room];
     store[data.batch].push(selected_user_data);
     await Store.setStore({ fep: store });
-  }
+    return resolve();
+  });
 };
 
 const update_store = async data => {
-  const store = await Store.getStore("fep");
-  if (Object.keys(batch_list).includes(data.batch)) {
-    const selected_user_data = [data.name, data.campus, data.room];
-    store[data.batch][parseInt(data.num, 10) - 1] = selected_user_data;
+  return new Promise(async (resolve, reject) => {
+    if (!Object.keys(batch_list).includes(data.batch)) {
+      return reject("Not in a proper batch");
+    }
+
+    const store = await Store.getStore("fep");
+    const selectedUserData = [data.name, data.campus, data.room];
+    store[data.batch][parseInt(data.num, 10) - 1] = selectedUserData;
     await Store.setStore({ fep: store });
-  }
+    return resolve();
+  });
 };
 
 const delete_store = async data => {
