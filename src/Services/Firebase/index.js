529d9ca9c1a852f862bb969c2106334b25816972
Glitch (fep-bot) <none>
2019-12-07 21:48:59 +0000

    🍁🎐 Checkpoint ./src/Services/Firebase/index.js:5658260/2847

--- src/Services/Firebase/index.js
+++ src/Services/Firebase/index.js
@@ -1,15 +1,22 @@
 import admin from "firebase-admin";
 import {firebaseConfig, firebaseAdminConfig, firebaseDataBaseURL} from "../../Config/Firebase";
 import fs from "fs-extra";
+import path from "path";
+import cp from "child_process";
 
 class Firebase {
   constructor(config) {
-    this.app = admin.initializeApp({
-      credential: admin.credential.cert(JSON.stringify(firebaseAdminConfig)),
-      dataBaseURL: firebaseDataBaseURL
-    });
+    this.app = this.initApp(config);
     this.db = this.app.firestore();
     this.initSetting();
+  }
+
+  initApp(config) {
+    const serviceAccountKeyFile = "serviceAccountKey.json";
+    const serviceAccountKeyPath = path.join(__dirname, "../../../../src/Bot/Config", `${serviceAccountKeyFile}`);
+    const serviceAccountKeyData = JSON.stringify(config.firebaseAdminConfig);
+    fs.writeFile(serviceAccountKeyFile, serviceAccountKeyData, "utf8");
+    return admin.initializeApp({credential: admin.credential.cert(serviceAccountKeyPath), dataBaseURL: config.firebaseDataBaseURL});
   }
 
   initSetting() {
@@ -21,4 +28,4 @@ class Firebase {
   }
 }
 
-export default new Firebase(firebaseConfig);
+export default new Firebase({firebaseConfig, firebaseDataBaseURL});
